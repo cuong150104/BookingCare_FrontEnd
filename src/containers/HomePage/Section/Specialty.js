44f45c7764ea48d8f9e1cbac7c1cb9c735e3f310
cuong150104 <nguyenduccuong01152004@gmail.com>
2024-09-04 23:21:59 +0700

    #49 Customize React-Slider-Carousel In React App

--- src/containers/HomePage/Section/Specialty.js
+++ src/containers/HomePage/Section/Specialty.js
@@ -4,40 +4,55 @@ import Slider from "react-slick";
 import './Specialty.scss'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-
+import specialtyImg from '../../../assets/specialty/co-xuong-khop.jpeg';
 class Specialty extends Component {
     render() {
         let settings = {
             dots: false,
             infinite: true,
             speed: 500,
-            slidesToShow: 3,
+            slidesToShow: 4,
             slidesToScroll: 1
-          };
+        };
         return (
             <div className='section-specialty'>
                 <div className='specialty-content'>
                     <div className="image-slider-container">
-                        <Slider {...settings}>
-                            <div>
-                                <h3>1</h3>
+                        <div className='specialty-container'>
+                            <div className='specialty-header'>
+                                <h4 className=''>Chuyên khoa phổ biến</h4>
+                                <button >Xem thêm</button>
                             </div>
-                            <div>
-                                <h3>2</h3>
+                            <div className='specialty-body'>
+                                <Slider {...settings}>
+                                    <div className='img-customize'>
+                                        <img src={specialtyImg} className='specialtyImg' />
+                                        <h3>Cơ xương khớp</h3>
+                                    </div>
+                                    <div className='img-customize'>
+                                        <img src={specialtyImg} className='specialtyImg' />
+                                        <h3>Cơ xương khớp</h3>
+                                    </div>
+                                    <div className='img-customize'>
+                                        <img src={specialtyImg} className='specialtyImg' />
+                                        <h3>Cơ xương khớp</h3>
+                                    </div>
+                                    <div className='img-customize'>
+                                        <img src={specialtyImg} className='specialtyImg' />
+                                        <h3>Cơ xương khớp</h3>
+                                    </div>
+                                    <div className='img-customize'>
+                                        <img src={specialtyImg} className='specialtyImg' />
+                                        <h3>Cơ xương khớp</h3>
+                                    </div>
+                                    <div className='img-customize'>
+                                        <img src={specialtyImg} className='specialtyImg' />
+                                        <h3>Cơ xương khớp</h3>
+                                    </div>
+                                </Slider>
                             </div>
-                            <div>
-                                <h3>3</h3>
-                            </div>
-                            <div>
-                                <h3>4</h3>
-                            </div>
-                            <div>
-                                <h3>5</h3>
-                            </div>
-                            <div>
-                                <h3>6</h3>
-                            </div>
-                        </Slider>
+                        </div>
+
                     </div>
                 </div>
             </div>
